235adcd31a2d27106bd02e5de3b8c5a57752044c
Ash Entwisle <ash.entwisle@mailfence.com>
2022-12-04 19:11:39 +0000

    updated d3t2
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/days/day03/task_two.rs
+++ src/days/day03/task_two.rs
@@ -1,31 +1,11 @@
-use std::collections::HashSet;
 
 pub fn run(data: &String) -> i32 {
-    // init variables
-    let mut total: i32 = 0;
-    let mut sets: Vec<HashSet<char>> = vec![
-        HashSet::new(),
-        HashSet::new(),
-        HashSet::new(),
-    ];
-
-    // loop over the lines
-    for (index, line) in data.lines().enumerate() {
-
-        // set the coresponding set to a set of the current line
-        sets[index % 3] = line.chars().collect();
-
-        // if the index is 3, check the sets
-        if index % 3 == 2 {
-            for character in sets[0].intersection(&sets[1]).cloned() {
-                if sets[2] .contains(&character) {
-                    total += get_priority(character);
-                }
-            }
-        }
-    }
-
-    total
+    data.lines().collect::<Vec<&str>>().chunks(3)
+        .map(|chunk| chunk[0].chars()
+            .find(|c| chunk[1].contains(*c) && chunk[2].contains(*c))
+            .unwrap())
+        .map(|c| get_priority(c))
+        .sum::<i32>()
 }
 
 fn get_priority(item: char) -> i32 {
