3efef49fbf539a3417d5b221928a793e2217566d
Ash Entwisle <ash.entwisle@mailfence.com>
2022-12-04 21:28:28 +0000

    updated d4, TODO: all readmes
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/days/day04/task_one.rs
+++ src/days/day04/task_one.rs
@@ -1,40 +1,14 @@
-use std::collections::HashSet;
-
 
 pub fn run(data: &String) -> i32 {
-    
-    // init variables
-    let mut total: i32 = 0;
-    let mut elf1: HashSet<i32>;
-    let mut elf2: HashSet<i32>;
-    
-    // loop over the lines
-    for line in data.lines() {
-        // split line at comma or -
-        let split: Vec<&str> = line.split(|c| c == ',' || c == '-').collect();
-
-        // generate sets
-        elf1 = generate_set(
-            split[0].parse().unwrap(), 
-            split[1].parse().unwrap(),
-        );
-        elf2 = generate_set(
-            split[2].parse().unwrap(), 
-            split[3].parse().unwrap(),
-        );
-
-        // check if one of the sets is a subset of the other
-        if elf1.union(&elf2).eq(&elf1) || elf1.union(&elf2).eq(&elf2) {
-            total += 1;
-        }
-    }
-    total
-}
-
-fn generate_set(start: i32, end: i32) -> HashSet<i32> {
-    let mut set: HashSet<i32> = HashSet::new();
-    for i in start..=end { set.insert(i); }
-    set
+    data.lines()
+        .map(|line: &str| line.split(|c| c == ',' || c == '-').collect())
+        .map(|split: Vec<&str>| split.iter()
+            // map each string to an integer
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>())
+        .map(|v: Vec<i32>| (v[0], v[1], v[2], v[3]))
+        .filter(|(a, b, c, d)| (a >= c && b <= d) || (a <= c && b >= d))
+        .count() as i32
 }
 
 
--- src/days/day04/task_two.rs
+++ src/days/day04/task_two.rs
@@ -1,40 +1,15 @@
-use std::collections::HashSet;
-
 
 pub fn run(data: &String) -> i32 {
-    
-    // init variables
-    let mut total: i32 = 0;
-    let mut elf1: HashSet<i32>;
-    let mut elf2: HashSet<i32>;
-    
-    // loop over the lines
-    for line in data.lines() {
-        // split line at comma or -
-        let split: Vec<&str> = line.split(|c| c == ',' || c == '-').collect();
-
-        // generate sets
-        elf1 = generate_set(
-            split[0].parse().unwrap(), 
-            split[1].parse().unwrap(),
-        );
-        elf2 = generate_set(
-            split[2].parse().unwrap(), 
-            split[3].parse().unwrap(),
-        );
-        
-        // check if there is an intersection
-        if elf1.intersection(&elf2).count() > 0 {
-            total += 1;
-        }
-    }
-    total
+    data.lines()
+        .map(|line: &str| line.split(|c| c == ',' || c == '-').collect())
+        .map(|split: Vec<&str>| split.iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>())
+        .map(|v: Vec<i32>| (v[0], v[1], v[2], v[3]))
+        .filter(|(a, b, c, d)| 
+            (a >= c && a <= d) || 
+            (b >= c && b <= d) || 
+            (a <= c && b >= c) || 
+            (a <= d && b >= d) )
+        .count() as i32
 }
-
-fn generate_set(start: i32, end: i32) -> HashSet<i32> {
-    let mut set: HashSet<i32> = HashSet::new();
-    for i in start..=end { set.insert(i); }
-    set
-}
-
-
