d45afdb30cd9e83a977a2eb4dbebdc3ac03d4503
Ash Entwisle <ash.entwisle@mailfence.com>
2022-12-04 17:02:30 +0000

    removed comments that dont need to be there
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/days/day01/task_one.rs
+++ src/days/day01/task_one.rs
@@ -1,8 +1,8 @@
 
 pub fn run(data: &String) -> u32 {    
-    data.split("\r\n\r\n")                              // split into groups
-        .map(|group| group.lines()                      // for each line in each group
-            .map(|line| line.parse::<u32>().unwrap())   // parse each line into a u32
-            .sum())                                     // sum the lines
-        .max().unwrap()                                 // get the max group                                                
+    data.split("\r\n\r\n")                              
+        .map(|group| group.lines()                      
+            .map(|line| line.parse::<u32>().unwrap())   
+            .sum())                                     
+        .max().unwrap()                                                                                 
 }
--- src/days/day01/task_two.rs
+++ src/days/day01/task_two.rs
@@ -1,10 +1,10 @@
 
 pub fn run(data: &String) -> u32 {  
-    let mut data = data.split("\r\n\r\n")               // split into groups                    
-        .map(|group| group.lines()                      // for each line in each group
-            .map(|line| line.parse::<u32>().unwrap())   // parse each line into a u32
-            .sum())                                     // sum the lines     
-        .collect::<Vec<u32>>();                         // collect into a vector
-    data.sort();                                        // sort the vector  (rust... why?)
-    data.into_iter().rev().take(3).sum::<u32>()         // return the sum of the 3 largest groups
+    let mut data = data.split("\r\n\r\n")                               
+        .map(|group| group.lines()                      
+            .map(|line| line.parse::<u32>().unwrap())   
+            .sum())                                          
+        .collect::<Vec<u32>>();                         
+    data.sort();                                        
+    data.into_iter().rev().take(3).sum::<u32>()         
 }
